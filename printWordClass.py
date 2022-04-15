ad70f701e6b77c12792fa8fa5c52c5c4a2901cca
Darien <darien2105@gmail.com>
2022-04-15 19:20:42 +1000

    Update printWordClass.py

--- printWordClass.py
+++ printWordClass.py
@@ -1,7 +1,12 @@
-import re; # Import regex, because you didn't say I couldn't, was simply asked for a "validation plan".
+import re; # Import regex, because you didn't say I couldn't 😉
+
+# Capital letter bools is a dumb implimentation
+true = True; # True to true
+false = False; # False to false
 
 # Good code doesn't need to be commented, bad/ hard to read code should always be commented.
 # But the requirements said to comment to explain things. So here we are with an overcommented file
+# Mostly because the requirements are fairly vague on commenting requirements
 
 # Ternary function
 def ternary(condition: bool, trueResult: any, falseResult: any) -> any:
@@ -15,20 +20,25 @@ def ternary(condition: bool, trueResult: any, falseResult: any) -> any:
 
 class GetLetterString:
 	"""
-	Class for building letters & words.
+	Class for creating letters & words.
 	"""
-	# There wasn't really a reason to wrap these in a class, but I did it anyway.
-	# Actually that's a lie, I did it because if I didn't, the best way to call the functions would
-	# have been to use eval() or a large number of if statements, which creates potential security risks/ code bloat.
-	# Doing it this way, we can call the functions by using getattr().
 
-	# This also allows for expansion of the class to include more letters if needed. (Always a good thing for code maintainablity)
-	# Also cause it's a class it technically dives into OOP.
-	def __init__(this, word: str, fStr: str = "*", eStr: str = " ",  delimiter: str = "\n"):
+	# Using a class helps for dynamically calling whatever letter function we want instead of checking for any specific letter using a bunch of if statements.
+	# It also helps for the future if we want to add more letters or even potentially symbols.
+	# The letter functions also have an underscore in front to show they're used internally, consistant with python naming standards.
+
+	# __Init__ sets up the class
+	def __init__(this, word: str, fStr: str = "*", eStr: str = " ",  delimiter: str = "\n") -> None:
 		this.word = word;
 		this.fStr = fStr;
 		this.eStr = eStr;
 		this.delimiter = delimiter;
+
+		# If the word length is 1, then it's a single letter
+		if(len(word) == 1):
+			this.singleLetter = true;
+		else:
+			this.singleLetter = false;
 
 	# All the letter functions work on the same principle:
 	# 1. Create an array
@@ -43,7 +53,7 @@ class GetLetterString:
 	# 9. Return the full letter string
 
 	# Function for the letter C
-	def letterC(this) -> str:
+	def _C(this) -> str:
 		"""
 		Returns a string of the letter 'C'
 		0'  ***  '
@@ -84,13 +94,13 @@ class GetLetterString:
 
 			# End of while loop
 			letterArray.append(lineString);
-		# End of while loop	
 
+		# End of while loop	
 		return this.delimiter.join(letterArray);
 
 
 	# Function for the letter O
-	def letterO(this) -> str:
+	def _O(this) -> str:
 		"""
 		Returns a string of the letter 'O'
 		0'  ***  '
@@ -126,13 +136,13 @@ class GetLetterString:
 
 			# End of while loop
 			letterArray.append(lineString);
-		# End of while loop	
 
+		# End of while loop	
 		return this.delimiter.join(letterArray);
 
 
 	# Function for the letter L
-	def letterL(this) -> str:
+	def _L(this) -> str:
 		"""
 		Returns a string of the letter 'L'
 		0' *     '
@@ -168,13 +178,13 @@ class GetLetterString:
 
 			# End of while loop
 			letterArray.append(lineString);
-		# End of while loop	
 
+		# End of while loop	
 		return this.delimiter.join(letterArray);
 
 
 	# Function for the letter D
-	def letterD(this) -> str:
+	def _D(this) -> str:
 		"""
 		Returns a string of the letter 'D'
 		0' ***   '
@@ -215,13 +225,13 @@ class GetLetterString:
 
 			# End of while loop
 			letterArray.append(lineString);
-		# End of while loop	
 
+		# End of while loop	
 		return this.delimiter.join(letterArray);
 
 
 	# Function for the letter H
-	def letterH(this) -> str:
+	def _H(this) -> str:
 		"""
 		Returns a string of the letter 'H'
 		0' *   * '
@@ -257,13 +267,13 @@ class GetLetterString:
 
 			# End of while loop
 			letterArray.append(lineString);
-		# End of while loop	
 
+		# End of while loop	
 		return this.delimiter.join(letterArray);
 
 
 	# Function for the letter T
-	def letterT(this) -> str:
+	def _T(this) -> str:
 		"""
 		Returns a string of the letter 'T'
 		0' ***** '
@@ -299,9 +309,27 @@ class GetLetterString:
 			
 			# End of while loop
 			letterArray.append(lineString);
-		# End of while loop	
 
+		# End of while loop	
 		return this.delimiter.join(letterArray);
+
+
+	# Function to build each letter
+	def _buildLetter(this, obj: dict, letterString: str) -> None:
+		"""
+		Gets each letter of the specified word,
+		line by line and applys each line to
+		the relevent line key in an object
+		"""
+		array = letterString.split(this.delimiter);
+		lineNumber = 0;
+
+		# Loop through the array,
+		# concat each string into the key of the object
+		string: str;
+		for string in array:
+			obj[lineNumber] += string;
+			lineNumber += 1;
 
 
 	# Function to call each letter function in order, then add each objects property to a string
@@ -312,54 +340,27 @@ class GetLetterString:
 		(Pun intended) the entire word together.
 		"""
 
-
-		# Function to build each letter
-		def buildLetter(obj: dict, array: str) -> None:
-			"""
-			Gets each letter of the specified word,
-			line by line and applys each line to
-			the relevent line key in an object
-			
-			This is an inner function, so that it cannot
-			be directly called outside of the class
-			"""
-			array = array.split(this.delimiter);
-			lineNumber = 0;
-
-			# Loop through the array,
-			# concat each string into the key of the object
-			string: str;
-			for string in array:
-				obj[lineNumber] += string;
-				lineNumber += 1;
-				
-
+		# If singleLetter is true, then we only want to build that letter
+		# Technically speeds up processing by not having to call buildLetter
+		# Also this is requirement as per the assignment
+		if(this.singleLetter):
+			letterFunction = getattr(this, f"_{this.word}");
+			return letterFunction()		
 
 		wordObject = { 0: "", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "" };
 		# For each letter in the word, find the appropriate function
 		letter: str;
 		for letter in this.word:
-			functionName = f"letter{letter}";
+			letterFunction = getattr(this, f"_{letter}");
+			letterString = letterFunction();
 
-			letterFunction = getattr(this, functionName);
-			letterArray = letterFunction();
-
-			buildLetter(wordObject, letterArray); # Taking advantage of objects being a memory reference
+			this._buildLetter(wordObject, letterString); # Taking advantage of objects being a memory reference
 		
 		return this.delimiter.join(wordObject.values());
 
 
-	# Destructor (Not really needed but added out of proof of knowledge)
-	# As such this just uses the pass keyword
-	def __del__(self):
-		pass
-
-
 # Main Function
-def main():
-	true = True;
-	false = False;
-
+def main() -> None:
 	# Toggle this to true to make the output easier to read
 	# (though it might actually make it harder if your console output doesn't support emojis)
 	readabilityMode = false;
@@ -376,43 +377,27 @@ def main():
 	# While(true) is considered bad, recursion would probably have been better
 	# Buuuuut we could possibly run into a Stack Overflow error
 	while(true):
-		"""
-		Asks the user for an input word, then checks it against several conditions.
-		If the input fails any of the checks, an error is printed and this function is called again.
-		If the input passes all the checks, builds the word, then calls this function again.
-		If the word is 'end', exits the function entirely.
-		"""
 		# Ask the user for a word/ letter
 		inputData = input("Please enter a word (Type \"end\" to exit): ");
 
-		# If the input was empty, go to next iteration
+		# If the inputData is empty, print an error and go to next iteration
 		if(inputData == ""):
 			print("Error: Input was empty");
 			continue; # Reset
 
-		# If the input word is end, break
+		# If inputData is "end", break
 		# Specified to be case insensitive
 		if(inputData.lower() == "end"):
 			break; # End
 
-		# If the word contains any characters other than "C, O, L, D, H, T", go to next iteration
+		# If the word contains any characters other than "COLDHT", print an error and go to next iteration
 		# Was specified that this is case sensitive, so only capitals work - Using Regex to make life easy.
 		match = re.search("[^COLDHT]", inputData);
 		if(match):
 			print("Error: Input can only contain the letters: C, O, L, D, H & T");
 			continue; # Reset
-		
-		# If the word is a single letter, simply print the relevant letter using the relevent function
-		# Then go to the next iteration (Technically this is a pointless check, but it's here as per the requirements)
-		if(len(inputData) == 1):
-			functionName = f"letter{inputData}";
-			
-			classItem = GetLetterString(inputData, fStr, eStr, delimiter);
-			letterFunction = getattr(classItem, functionName);
-			letter = letterFunction()
 
-			print(letter);
-			continue; # Reset
+		inputData = inputData.upper();
 		
 		# All checks passed, build the word
 		word = GetLetterString(inputData, fStr, eStr, delimiter).buildWord();
@@ -421,5 +406,6 @@ def main():
 
 
 # Call the main function - Also prevents the program from running if this file is imported
+# Specifically because this script is making use of an infinite while loop. Wouldn't want that running if imported.
 if(__name__ == "__main__"):
 	main();
